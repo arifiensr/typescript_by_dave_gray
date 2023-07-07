32a5aa919cea3b96b5ad8a9fe81b866ef65cb8a4
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-07-07 20:49:31 +0700

    Lesson 5 (DNF)

--- LESSON05/build/js/main.js
+++ LESSON05/build/js/main.js
@@ -3,5 +3,21 @@
 let a = 'hello';
 let b = a; // less specific
 let c = a; // more specific
-let d = ' world';
-let e = ' world';
+let d = 'world';
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+let myVal = addOrConcat(2, 2, 'concat');
+// ! Be careful! TS sees no problem - but a string is returned
+let nextVal = addOrConcat(2, 2, 'concat');
+// 10 as string
+10;
+// ! The DOM
+const img = document.querySelector('img'); // '!' is non-null element
+const myImg = document.getElementById('#img');
+const nextImg = document.getElementById('#img');
+img.src;
+myImg.src;
